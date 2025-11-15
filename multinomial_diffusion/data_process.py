1212ce1f3f0ec438fbd86c712635b795f58b0b18
zj13579 <49724963+zj13579@users.noreply.github.com>
2025-11-15 11:46:57 +0800

    Update data_process.py

--- multinomial_diffusion/data_process.py
+++ multinomial_diffusion/data_process.py
@@ -99,6 +99,32 @@ def get_data(args):
         data = data.view(6040, 1, 34, 109)
 
         dataset_train = data
-        train_loader = DataLoader(dataset_train, batch_size=32, shuffle=True, num_workers=0, pin_memory=False)
+        train_loader = DataLoader(dataset_train, batch_size=64, shuffle=True, num_workers=0, pin_memory=False)
+
+    if args.dataset == 'ml-10m':
+        data_shape = (1, 33, 985)
+        num_classes = 6
+    
+        num_users = 35785
+        num_items = 32525
+    
+        rating_matrix = np.zeros((num_users, num_items))
+    
+        with open('MDPAttack/datasets/ml-10m/train_data.txt', "r") as file:
+            for line in file:
+                user, item, rating = map(str, line.strip().split())
+                user = int(user)
+                item = int(item)
+                rating = float(rating)
+                rating_matrix[user, item] = rating
+    
+        last_one_columns = rating_matrix[:, -20:]
+        rating_matrix = np.hstack((rating_matrix, last_one_columns))
+    
+        data = torch.from_numpy(rating_matrix).int()
+        data = data.view(35785, 1, 33, 985)
+    
+        dataset_train = data
+        train_loader = DataLoader(dataset_train, batch_size=128, shuffle=True, num_workers=0, pin_memory=False)
 
     return train_loader, data_shape, num_classes
